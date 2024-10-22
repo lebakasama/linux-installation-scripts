b0df46a8d905282b3b688d1373a3bf13de713a7b
Franck Caniard <github@le-franck.com>
2024-10-22 21:19:13 +0900

    Added first version of script for Manjaro

---
+++ manjaro/install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+PACMAN_INSTALL=sudo pacman -S --no-confirm
+YAY_INSTALL=yay -S --no-confirm
+
+# Install base packages and packages required for later stages
+$PACMAN_INSTALL \
+    base-devel \
+    git \
+    linux-headers-meta \
+    vim
+
+# Install yay
+mkdir ~/tmp
+cd ~/tmp
+git clone https://aur.archlinux.org/yay-git.git
+cd yay-git
+makepkg -si
+cd
+rm -rm tmp/yay-git
+
+# DisplayLink hub drivers
+$YAY_INSTALL evdi displaylink
+
+# Power management
+$PACMAN_INSTALL tlp thermald
+sudo systemctl start tlp
+sudo systemctl enable tlp
+sudo systemctl start thermald
+sudo systemctl enable thermald
+
+# Set kernel parameters to enable suspend
+sudo sed -i '6s/"$/ acpi_rev_override=1 acpi_osi=Linux mem_sleep_default=deep"/' /etc/default/grub
+
+# Regenerate grub settings
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
