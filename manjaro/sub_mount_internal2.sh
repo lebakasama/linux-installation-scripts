df16910947ce90cd3eb26272d3849cf90fbbf149
Franck Caniard <github@le-franck.com>
2025-01-22 15:49:09 +0900

    Added second drive's mounting script

---
+++ manjaro/sub_mount_internal2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+INTERNAL2_DEV=/dev/nvme0n1p1
+INTERNAL2_MAP=internal2
+INTERNAL2_KEY=/home/franck/.config/internal2_luksKey
+
+printf "\n/dev/mapper/$INTERNAL2_MAP\t/mnt/$INTERNAL2_MAP\text4\tdefaults\t0\t0" >> /etc/fstab
+printf "\n$INTERNAL2_MAP\t$INTERNAL2_DEV\t$INTERNAL2_KEY" >> /etc/crypttab
+
+echo "And now reboot..."
+
